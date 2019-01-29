2b2a2bbabf38b89bc73e903e33121cf84a48fbae
ArupDawn <arupchandradawn@gmail.com>
2019-01-29 18:47:21 +0530

    Updates

---
+++ precache-manifest.fa84fd9ecb23690658a771548b269d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e239c9062e226c14dbdd",
+    "url": "/drum/static/js/runtime~main.e239c906.js"
+  },
+  {
+    "revision": "cc251bd32bb88d0692e1",
+    "url": "/drum/static/js/main.cc251bd3.chunk.js"
+  },
+  {
+    "revision": "ed916c7af8962308c854",
+    "url": "/drum/static/js/1.ed916c7a.chunk.js"
+  },
+  {
+    "revision": "cc251bd32bb88d0692e1",
+    "url": "/drum/static/css/main.f6627fef.chunk.css"
+  },
+  {
+    "revision": "ed916c7af8962308c854",
+    "url": "/drum/static/css/1.77b66ef2.chunk.css"
+  },
+  {
+    "revision": "0efd495d9160a38d7d1eebf8d3a9bac8",
+    "url": "/drum/index.html"
+  }
+];
